da4fcfcdcba27b08204e66cce4ebbec1a9a6e787
Talia Pradiana <talia.n.pradiana@gmail.com>
2023-07-18 14:35:53 -0500

    Initial Commit

---
+++ clarityProject/clarityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  clarityProjectApp.swift
+//  clarityProject
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct clarityProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
